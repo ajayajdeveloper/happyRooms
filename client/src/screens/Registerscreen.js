f228bbc8b6dfd18a4ac4f0ee2b9d7fd3884b017b
Ajaydev <ajaykumaraj2529@gmail.com>
2022-03-04 13:32:33 +0530

    my changes

--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {useState , useEffect} from "react"
+import { useState, useEffect } from "react"
 import axios from "axios"
 import Loader from '../components/Loader'
 import Error from '../components/Error'
 import Success from '../components/Success';
 
 function Registerscreen() {
-    const [name,setname] = useState(" ")
-    const [email,setemail] = useState(" ")
-    const [password,setpassword] = useState(" ")
-    const [cpassword,setcpassword] = useState(" ")
+    const [name, setname] = useState(" ")
+    const [email, setemail] = useState(" ")
+    const [password, setpassword] = useState(" ")
+    const [cpassword, setcpassword] = useState(" ")
 
     const [loading, setloading] = useState(false)
     const [error, seterror] = useState()
-    const [success,setsuccess] = useState()
+    const [success, setsuccess] = useState()
 
 
-   async  function register(){
+    async function register() {
 
-        if(password===cpassword)
-        {
+        if (password === cpassword) {
             const user = {
                 name,
                 email,
@@ -27,47 +26,46 @@ function Registerscreen() {
                 cpassword
             }
 
-            try{
-                    setloading(true)
-                    const result = await axios.post("https://happyrooms.herokuapp.com/api/users/register",user).data 
-                    setloading(false)
-                    setsuccess(true)
+            try {
+                setloading(true)
+                const result = await axios.post("https://happyrooms.herokuapp.com/api/users/register", user).data
+                setloading(false)
+                setsuccess(true)
 
-                    setemail('')
-                    setname('')
-                    setcpassword('')
-                    setpassword('')
+                setemail('')
+                setname('')
+                setcpassword('')
+                setpassword('')
 
             }
-            catch(error)
-            {
-               console.log(error)
-               setloading(false)
-               seterror(true)
+            catch (error) {
+                console.log(error)
+                setloading(false)
+                seterror(true)
             }
-           
+
         }
-        else{
+        else {
             alert("password not match")
         }
 
 
     }
 
-  return (
-      <div>
+    return (
+        <div>
 
-          {loading && (<Loader/>)}
-          {error && (<Error/>)}
-         
-          <div className='row justify-content-center mt-5'>
-              <div className='col-md-5 mt-5 '>
+            {loading && (<Loader />)}
+            {error && (<Error />)}
 
-              {success && <Success message="registertion success"/>}
+            <div className='row justify-content-center mt-5'>
+                <div className='col-md-5 mt-5 '>
 
-                  <div className='bs'>
-                      <h2>Register</h2>
-                      <input  type="text"  placeholder="name"  className='form-control' 
+                    {success && <Success message="registertion success" />}
+
+                    <div className='bs'>
+                        <h2>Register</h2>
+                        {/* <input  type="text"  placeholder="name"  className='form-control' 
                       value={name} onChange={ (e) => {setname(e.target.value)}} autofocus  />
 
                       <input type="text" placeholder='email' className='form-control' 
@@ -77,15 +75,35 @@ function Registerscreen() {
                       value={password} onChange={ (e) => {setpassword(e.target.value)}} autofocus/>
 
                       <input type="text" placeholder='confirm password' className='form-control' 
-                      value={cpassword} onChange={ (e) => {setcpassword(e.target.value)}} autofocus/>
+                      value={cpassword} onChange={ (e) => {setcpassword(e.target.value)}} autofocus/> */}
 
-                      <button className='btn btn-primary mt-3' onClick={register}>Register</button>
-                  </div>
 
-              </div>
-          </div>
-      </div>
-  )
+                        <input required type="text" placeholder="name" className="form-control mt-1" value={name} onChange={(e) => { setname(e.target.value) }} />
+                        <input required type="text" placeholder="email" className="form-control mt-1" value={email} onChange={(e) => { setemail(e.target.value) }} />
+                        <input
+                            type="text"
+                            placeholder="password"
+                            className="form-control mt-1"
+                            value={password}
+                            required
+                            onChange={(e) => { setpassword(e.target.value) }}
+                        />
+                        <input
+                            type="text"
+                            placeholder="confirm password"
+                            className="form-control mt-1"
+                            value={cpassword}
+                            required
+                            onChange={(e) => { setcpassword(e.target.value) }}
+                        />
+
+                        <button className='btn btn-primary mt-3' onClick={register}>Register</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
