620a04f5738451c97be71ad860e6bb6fb13bdd92
Ajaydev <ajaykumaraj2529@gmail.com>
2022-03-09 14:05:58 +0530

    app changes

--- client/src/screens/Landingscreen.js
+++ client/src/screens/Landingscreen.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import moment from "moment"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Link } from "react-router-dom";
 AOS.init({
-    duration: '2000'
+    duration:'2000'
 });
-
-
 function Landingscreen() {
-    return (
-        <div className='row landing justify-content-center'>
-
-            <div className='col-md-9 my-auto text-center' style={{ borderRight: "8px solid white" }}>
-                <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "130px" }}>Happy Rooms</h2>
+  return (
+    <div className="">
+      <div className="landing row justify-content-center text-center">
+        <div className="col-md-9 my-auto" style={{borderRight:'8px solid white'}}>
+        <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "130px" }}>Happy Rooms</h2>
                 <h1 data-aos="zoom-out" style={{ color: "white" }} > Your Happiest vactions !!! </h1>
-                <Link to="/home">
-                    <button className='btn landingbtn' style={{ color: "black" }}>Get Started</button>
-                </Link>
-
-            </div>
-
+          <Link to="/home">
+             <button className='btn btn-primary'>Get Started</button>
+          </Link>
         </div>
-    )
+      </div>
+     
+    </div>
+  );
 }
 
-export default Landingscreen
+export default Landingscreen;
 
 
