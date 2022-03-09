729c79322d71845df17a51247e3641ad17004173
Ajaydev <ajaykumaraj2529@gmail.com>
2022-03-09 13:47:29 +0530

    changes

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,69 +1,146 @@
-import React from 'react';
-import {useState , useEffect} from "react"
-import axios from "axios"
-import Loader from '../components/Loader'
-import Error from '../components/Error'
+// import React from 'react';
+// import {useState , useEffect} from "react"
+// import axios from "axios"
+// import Loader from '../components/Loader'
+// import Error from '../components/Error'
 
-function Loginscreen() {
+// function Loginscreen() {
     
-    const [email,setemail] = useState(" ")
-    const [password,setpassword] = useState(" ")
-    const [loading, setloading] = useState(false)
-    const [error, seterror] = useState()
+//     const [email,setemail] = useState(" ")
+//     const [password,setpassword] = useState(" ")
+//     const [loading, setloading] = useState(false)
+//     const [error, seterror] = useState()
 
-  async function Login(){
+//   async function Login(){
 
-        const user = {
+//         const user = {
              
-                email,
-                password,
+//                 email,
+//                 password,
                
-            }
-            try{
-                setloading(true)
-                const result = (await axios.post('https://happyrooms.herokuapp.com/api/users/login',user)).data
-                // setloading(false)
-                localStorage.setItem('currentUser',JSON.stringify(result))
-                window.location.href='/home'
-            }
-            catch(error)
-            {
-               console.log(error)
-               setloading(false)
-               seterror(true)
-            }
+//             }
+//             try{
+//                 setloading(true)
+//                 const result = (await axios.post('https://happyrooms.herokuapp.com/api/users/login',user)).data
+//                 // setloading(false)
+//                 localStorage.setItem('currentUser',JSON.stringify(result))
+//                 window.location.href='/home'
+//             }
+//             catch(error)
+//             {
+//                console.log(error)
+//                setloading(false)
+//                seterror(true)
+//             }
             
      
+//     }
+
+//   return (
+//       <div>
+//               {loading && (<Loader/>)}
+//           <div className='row justify-content-center mt-5'>
+//               <div className='col-md-5 mt-5 '>
+//                   {error && (<Error message=" invalid credentials"/>)}
+//                   <div className='bs'>
+//                       <h2>Login</h2>
+                   
+
+//                       <input type="text" placeholder='email'  className='form-control' 
+//                       value={email} onChange={ (e) => {setemail(e.target.value)}} />
+
+//                       <input type="text"  placeholder='password' className='form-control' 
+//                       value={password} onChange={ (e) => {setpassword(e.target.value)}}/>
+
+                   
+//                       <button className='btn btn-primary mt-3' onClick={Login}>Login</button>
+//                   </div>
+
+//               </div>
+//           </div>
+//       </div>
+//   )
+
+// }
+
+// export default Loginscreen;
+
+
+
+
+import React, { useState, useEffect } from "react";
+import {useDispatch , useSelector} from 'react-redux'
+import axios from "axios";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
+import Success from "../components/Success";
+import Swal from 'sweetalert2'
+export default function Loginscreen() {
+  
+
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const[loading, setloading]=useState(false)
+    const[error, seterror]=useState(false)
+    const[success, setsuccess]=useState(false)    
+
+    useEffect(() => {
+
+          if(localStorage.getItem('currentUser'))
+          {
+              window.location.href='/'
+          }
+        
+    }, [])
+
+    async function login(){
+      const user={
+     
+        email,
+        password
+    }
+      try {
+        setloading(true)
+        const result = await (await axios.post('/api/users/login',user)).data
+        localStorage.setItem('currentUser',JSON.stringify(result))
+        window.location.href='/'
+      } catch (error) {
+        seterror(true)
+        setloading(false)
+        console.log(error);
+        
+      }
     }
 
-  return (
-      <div>
-              {loading && (<Loader/>)}
-          <div className='row justify-content-center mt-5'>
-              <div className='col-md-5 mt-5 '>
-                  {error && (<Error message=" invalid credentials"/>)}
-                  <div className='bs'>
-                      <h2>Login</h2>
-                   
+    return (
+        <div className='login'>
+         <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
+          <h2 className="text-center m-2" style={{ fontSize: "35px" }}>
+            Login
+          </h2>
 
-                      <input type="text" placeholder='email'  className='form-control' 
-                      value={email} onChange={ (e) => {setemail(e.target.value)}} />
-
-                      <input type="text"  placeholder='password' className='form-control' 
-                      value={password} onChange={ (e) => {setpassword(e.target.value)}}/>
-
-                   
-                      <button className='btn btn-primary mt-3' onClick={Login}>Login</button>
-                  </div>
-
-              </div>
+          {loading && (<Loader/>)} 
+          {error && (<Error error='Invalid Credentials'/>)}
+          {success && (<Success success='User Login Successfull'/>)}
+          <div>
+            <input required type="text" placeholder="email" className="form-control mt-1" value={email} onChange={(e)=>{setemail(e.target.value)}} />
+            <input
+              type="text"
+              placeholder="password"
+              className="form-control mt-1"
+              value={password}
+              required
+              onChange={(e)=>{setpassword(e.target.value)}}
+            />
+            
+            <button onClick={login} className="btn btn-success mt-3 mb-3 rounded-pill">LOGIN</button>
+            <br/>
+            <a style={{color:'black'}} href="/register" className="mt-2">Click Here To Register</a>
           </div>
+        </div>
       </div>
-  )
-
+        </div>
+    )
 }
-
-export default Loginscreen;
-
-
 
--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -1,90 +1,180 @@
-import React from 'react';
-import { useState, useEffect } from "react"
-import axios from "axios"
-import Loader from '../components/Loader'
-import Error from '../components/Error'
-import Success from '../components/Success';
+// import React from 'react';
+// import { useState, useEffect } from "react"
+// import axios from "axios"
+// import Loader from '../components/Loader'
+// import Error from '../components/Error'
+// import Success from '../components/Success';
 
-function Registerscreen() {
-    const [name, setname] = useState(" ")
-    const [email, setemail] = useState(" ")
-    const [password, setpassword] = useState(" ")
-    const [cpassword, setcpassword] = useState(" ")
+// function Registerscreen() {
+//     const [name, setname] = useState(" ")
+//     const [email, setemail] = useState(" ")
+//     const [password, setpassword] = useState(" ")
+//     const [cpassword, setcpassword] = useState(" ")
 
-    const [loading, setloading] = useState(false)
-    const [error, seterror] = useState()
-    const [success, setsuccess] = useState()
-
-
-    async function register() {
-
-        if (password === cpassword) {
-            const user = {
-                name,
-                email,
-                password,
-                cpassword
-            }
-
-            try {
-                setloading(true)
-                const result = await axios.post("https://happyrooms.herokuapp.com/api/users/register", user).data
-                setloading(false)
-                setsuccess(true)
-
-                setemail('')
-                setname('')
-                setcpassword('')
-                setpassword('')
-
-            }
-            catch (error) {
-                console.log(error)
-                setloading(false)
-                seterror(true)
-            }
-
-        }
-        else {
-            alert("password not match")
-        }
+//     const [loading, setloading] = useState(false)
+//     const [error, seterror] = useState()
+//     const [success, setsuccess] = useState()
 
 
-    }
+//     async function register() {
 
-    return (
-        <div>
+//         if (password === cpassword) {
+//             const user = {
+//                 name,
+//                 email,
+//                 password,
+//                 cpassword
+//             }
 
-            {loading && (<Loader />)}
-            {error && (<Error />)}
+//             try {
+//                 setloading(true)
+//                 const result = await axios.post("https://happyrooms.herokuapp.com/api/users/register", user).data
+//                 setloading(false)
+//                 setsuccess(true)
 
-            <div className='row justify-content-center mt-5'>
-                <div className='col-md-5 mt-5 '>
+//                 setemail('')
+//                 setname('')
+//                 setcpassword('')
+//                 setpassword('')
 
-                    {success && <Success message="registertion success" />}
+//             }
+//             catch (error) {
+//                 console.log(error)
+//                 setloading(false)
+//                 seterror(true)
+//             }
 
-                    <div className='bs'>
-                        <h2>Register</h2>
-                        <input  type="text"  placeholder="name"  className='form-control' 
-                      value={name} onChange={ (e) => {setname(e.target.value)}} autofocus  />
+//         }
+//         else {
+//             alert("password not match")
+//         }
 
-                      <input type="text" placeholder='email' className='form-control' 
-                      value={email} onChange={ (e) => {setemail(e.target.value)}}autofocus />
 
-                      <input type="text"  placeholder='password' className='form-control' 
-                      value={password} onChange={ (e) => {setpassword(e.target.value)}} autofocus/>
+//     }
 
-                      <input type="text" placeholder='confirm password' className='form-control' 
-                      value={cpassword} onChange={ (e) => {setcpassword(e.target.value)}} autofocus/>
+//     return (
+//         <div>
 
-                        <button className='btn btn-primary mt-3' onClick={register}>Register</button>
-                    </div>
+//             {loading && (<Loader />)}
+//             {error && (<Error />)}
 
-                </div>
-            </div>
+//             <div className='row justify-content-center mt-5'>
+//                 <div className='col-md-5 mt-5 '>
+
+//                     {success && <Success message="registertion success" />}
+
+//                     <div className='bs'>
+//                         <h2>Register</h2>
+//                         <input  type="text"  placeholder="name"  className='form-control' 
+//                       value={name} onChange={ (e) => {setname(e.target.value)}} autofocus  />
+
+//                       <input type="text" placeholder='email' className='form-control' 
+//                       value={email} onChange={ (e) => {setemail(e.target.value)}}autofocus />
+
+//                       <input type="text"  placeholder='password' className='form-control' 
+//                       value={password} onChange={ (e) => {setpassword(e.target.value)}} autofocus/>
+
+//                       <input type="text" placeholder='confirm password' className='form-control' 
+//                       value={cpassword} onChange={ (e) => {setcpassword(e.target.value)}} autofocus/>
+
+//                         <button className='btn btn-primary mt-3' onClick={register}>Register</button>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div>
+//     )
+
+// }
+
+// export default Registerscreen;
+
+
+
+
+import React, { useState, useEffect } from "react";
+import {useDispatch , useSelector} from 'react-redux'
+import axios from "axios";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
+import Success from '../components/Success'
+export default function Registerscreen() {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [cpassword, setcpassword] = useState("");
+  const[loading, setloading]=useState(false)
+  const[error, seterror]=useState(false)
+  const[success, setsuccess]=useState(false) 
+  async function register(){
+
+      if(password!=cpassword)
+      {
+          alert("passwords not matched")
+      }
+      else{
+          const user={
+              name,
+              email,
+              password
+          }
+          
+          try {
+            setloading(true)
+            const result = await axios.post('/api/users/register',user)
+            setloading(false)
+            setsuccess(true)
+            setemail('')
+            setname('')
+            setcpassword('')
+            setpassword('')
+          } catch (error) {
+            seterror(true)
+            setloading(false)
+            console.log(error);
+          }
+      
+      }
+
+  }
+
+  return (
+    <div className='register'>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
+
+          {loading && (<Loader/>)}
+          {success && (<Success success='User Registered Successfully' />)}
+          {error && (<Error error='Email already registred' />)}
+
+          <h2 className="text-center m-2" style={{ fontSize: "35px" }}>
+            Register
+          </h2>
+          <div>
+            <input required type="text" placeholder="name" className="form-control mt-1" value={name} onChange={(e)=>{setname(e.target.value)}} />
+            <input required type="text" placeholder="email" className="form-control mt-1" value={email} onChange={(e)=>{setemail(e.target.value)}} />
+            <input
+              type="text"
+              placeholder="password"
+              className="form-control mt-1"
+              value={password}
+              required
+              onChange={(e)=>{setpassword(e.target.value)}}
+            />
+            <input
+              type="text"
+              placeholder="confirm password"
+              className="form-control mt-1"
+              value={cpassword}
+              required
+              onChange={(e)=>{setcpassword(e.target.value)}}
+            />
+            <button onClick={register} className="btn btn-primary rounded-pill mt-3 mb-3">REGISTER</button>
+            <br/>
+            <a style={{color:'black'}} href="/login">Click Here To Login</a>
+          </div>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
-
-export default Registerscreen;
